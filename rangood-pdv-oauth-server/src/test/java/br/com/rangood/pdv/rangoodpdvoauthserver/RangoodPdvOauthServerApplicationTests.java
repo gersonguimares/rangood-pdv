ae26a99779fe56ad9b70e45605a7eea144ef43ae
gersonguimaraes <gerson.mfg@gmail.com>
2021-06-21 02:34:40 -0300

    - Criação do microserviço OAuth para autenticação

---
+++ rangood-pdv-oauth-server/src/test/java/br/com/rangood/pdv/rangoodpdvoauthserver/RangoodPdvOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rangood.pdv.rangoodpdvoauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangoodPdvOauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

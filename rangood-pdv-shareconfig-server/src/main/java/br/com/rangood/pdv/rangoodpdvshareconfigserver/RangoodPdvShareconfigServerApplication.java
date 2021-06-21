1a2dcb276166cb30c6eb9548c55768a602da0aef
gersonguimaraes <gerson.mfg@gmail.com>
2021-06-21 00:56:56 -0300

    - Configuração do servidor de configuração centralizada

---
+++ rangood-pdv-shareconfig-server/src/main/java/br/com/rangood/pdv/rangoodpdvshareconfigserver/RangoodPdvShareconfigServerApplication.java
@@ -0,0 +1,15 @@
+package br.com.rangood.pdv.rangoodpdvshareconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class RangoodPdvShareconfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangoodPdvShareconfigServerApplication.class, args);
+	}
+
+}
---
+++ rangood-pdv-shareconfig-server/src/test/java/br/com/rangood/pdv/rangoodpdvshareconfigserver/RangoodPdvShareconfigServerApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.rangood.pdv.rangoodpdvshareconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangoodPdvShareconfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}

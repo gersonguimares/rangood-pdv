7dfb3f9593685b2676f1f28537af45b73bdced91
gersonguimaraes <gerson.mfg@gmail.com>
2021-06-21 01:00:52 -0300

    - Configuração base do servidor API Gateway

---
+++ rangood-pdv-apigateway-server/src/test/java/br/com/rangood/pdv/rangoodpdvapigatewayserver/RangoodPdvApigatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rangood.pdv.rangoodpdvapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangoodPdvApigatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
